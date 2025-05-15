baa10bd270a108d1e239baa946da948768c4524d
Bash-Abdul <bashirtaiwo59@gmail.com>
2025-05-15 16:11:55 +0100

    project page updates

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -3,9 +3,11 @@ import project_image from './page.png'
 import clabed from './clabed.png'
 import bloggle from './bloggle.png'
 import { useState, useEffect } from "react";
-import {sanityClient} from '../../sanityClient'
+import { sanityClient } from '../../sanityClient'
 import { TbBrandGithub } from "react-icons/tb";
 import { AiFillEye } from "react-icons/ai";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 // const projectData = [
 //   {
@@ -39,12 +41,12 @@ import { AiFillEye } from "react-icons/ai";
 // ];
 
 const Projects = () => {
-    const [projects, setProjects] = useState([]);
+  const [projects, setProjects] = useState([]);
 
-    useEffect(() => {
-        sanityClient
-          .fetch(
-            `*[_type == "project"]{
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "project"]{
               title,
               subtitle,
               technology,
@@ -58,13 +60,13 @@ const Projects = () => {
                 alt
               }
             }`
-          )
-          .then((data) => {
-            console.log("Fetched Projects:", data);
-            setProjects(data);
-          })
-          .catch(console.error);
-      }, []);
+      )
+      .then((data) => {
+        console.log("Fetched Projects:", data);
+        setProjects(data);
+      })
+      .catch(console.error);
+  }, []);
   return (
     <div>
       <div className="mt-[3rem] grid grid-cols-3 gap-[2rem]">
@@ -72,7 +74,12 @@ const Projects = () => {
           <div className="shadow-2xl" key={index}>
             {/* <img src={project.mainImage} alt={project.mainImage.alt} /> */}
             {project.mainImage && (
-              <img src={project.mainImage.asset.url} alt={project.name} />
+              // <img src={project.mainImage.asset.url} alt={project.name} />
+              <LazyLoadImage
+              src={project.mainImage.asset.url}
+              alt={project.name}
+              effect="blur"
+              />
             )}
             <div className="text-white py-10 px-5 flex gap-4 flex-col">
               <h1>{project.title}</h1>
@@ -83,13 +90,13 @@ const Projects = () => {
 
               <div className="flex items-center justify-start gap-6 text-sm text-[#B2B2B3] cursor-pointer">
                 <div className="flex items-center gap-2 hover:scale-[1.1] transition">
-                    <TbBrandGithub className="fill-white" />
-                <a href={project.codeLink} target="_blank" rel="noopener noreferrer" className="">Code</a>
+                  <TbBrandGithub className="fill-white" />
+                  <a href={project.codeLink} target="_blank" rel="noopener noreferrer" className="">Code</a>
                 </div>
-               <div className="flex items-center gap-2 hover:scale-[1.1] transition">
-                <AiFillEye className="text-white" />
-               <a href={project.viewLink} target="_blank" rel="noopener noreferrer" className="">View live</a>
-               </div>
+                <div className="flex items-center gap-2 hover:scale-[1.1] transition">
+                  <AiFillEye className="text-white" />
+                  <a href={project.viewLink} target="_blank" rel="noopener noreferrer" className="">View live</a>
+                </div>
               </div>
             </div>
           </div>
